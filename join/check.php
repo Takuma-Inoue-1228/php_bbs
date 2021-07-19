cfafcd510b93ba860c256b3318662406416018d2
TakumaInoue <inouetakuma19921228@gmail.com>
2021-07-19 23:08:16 +0900

    add_User_registration_function

--- join/check.php
+++ join/check.php
@@ -1,10 +1,26 @@
 <?php
 session_start();
+require('../dbconnect.php');
 
 if (!isset($_SESSION['join'])) {
 	header('Location: index.php');
 	exit();
 }
+
+if (!empty($_POST)) {
+	$statement = $db->prepare('INSERT INTO members SET name=?, email=?, password=?, picture=?, created=NOW()');
+	echo $statement->execute(array(
+		$_SESSION['join']['name'],
+		$_SESSION['join']['email'],
+		sha1($_SESSION['join']['password']),
+		$_SESSION['join']['image']
+	));
+	unset($_SESSION['join']);
+
+	header('Location: thanks.php');
+	exit();
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -44,8 +60,8 @@ if (!isset($_SESSION['join'])) {
 					</dd>
 					<dt>写真など</dt>
 					<dd>
-						<?php if ($_SESSION['join']['image'] !== ''): ?>
-							<img  class="picture" src="../member_picture/<?php print(htmlspecialchars($_SESSION['join']['image'], ENT_QUOTES)); ?>">
+						<?php if ($_SESSION['join']['image'] !== '') : ?>
+							<img class="picture" src="../member_picture/<?php print(htmlspecialchars($_SESSION['join']['image'], ENT_QUOTES)); ?>">
 						<?php endif; ?>
 					</dd>
 				</dl>
